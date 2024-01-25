e5edee24504080ba46197e51b488cd091f91703e
eparascho <eparascho@csd.auth.gr>
2024-01-25 17:16:49 +0200

    hdbscan training features visualization

--- clustering/clustering_visualization.py
+++ clustering/clustering_visualization.py
@@ -2,10 +2,8 @@
 This file contains the code for visualizing the clustering results.
 """
 
-
 import pandas as pd
 import matplotlib.pyplot as plt
-
 
 if __name__ == '__main__':
     # read raw data
@@ -17,17 +15,17 @@ if __name__ == '__main__':
     hdbscan = pd.read_csv('../data/clustering_results/hdbscan_results.csv')
     hdbscan.drop(hdbscan.columns[0], axis=1, inplace=True)  # drop the first column
     hdbscan.columns = column_names  # rename columns
+    hdbscan.rename(columns={'WORK/SCHOOL': 'WORK_SCHOOL'}, inplace=True)
 
-    boxplot_data = [hdbscan[hdbscan['cluster'] == cluster]['exercise_duration'] for cluster in hdbscan['cluster'].unique()]
-    plt.figure(figsize=(8, 6))
-
-    # Colors for each box
-    colors = ['#ffcc99', '#ccccff', '#ffffcc', '#ccffe6', '#b3e6ff']
-    boxplot_elements = plt.boxplot(boxplot_data, patch_artist=True, labels=hdbscan['cluster'].unique())
-    for patch, color in zip(boxplot_elements['boxes'], colors):
-        patch.set_facecolor(color)
-
-    plt.title('Box Plot by Cluster')
-    plt.xlabel('Cluster')
-    plt.ylabel('Feature Values')
-    plt.show()
+    cols = list(hdbscan.columns[1:37])
+    for col in cols:
+        boxplot_data = [hdbscan[hdbscan['cluster'] == cluster][col] for cluster in hdbscan['cluster'].unique()]
+        plt.figure(figsize=(8, 6))
+        colors = ['#ffcc99', '#ccccff', '#ffffcc', '#ccffe6', '#b3e6ff']
+        boxplot_elements = plt.boxplot(boxplot_data, patch_artist=True, labels=hdbscan['cluster'].unique())
+        for patch, color in zip(boxplot_elements['boxes'], colors):
+            patch.set_facecolor(color)
+        plt.xlabel('Cluster')
+        plt.ylabel(col)
+        filename = '../images/clustering/hdbscan_' + col + '.png'
+        plt.savefig(filename)
