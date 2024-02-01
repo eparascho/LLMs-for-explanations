75a0a40e45163e52b6d4db34ab947fde70b5e616
eparascho <eparascho@csd.auth.gr>
2024-02-01 18:46:55 +0200

    labeling visualizations v1

--- clustering/clustering_visualization.py
+++ clustering/clustering_visualization.py
@@ -2,30 +2,77 @@
 This file contains the code for visualizing the clustering results.
 """
 
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
-    # read raw data
-    data = pd.read_pickle('../data/clustering_df.pkl')
-    column_names = data.columns.values.tolist()  # raw data column names
-    column_names.append('cluster')  # cluster
 
-    # read and prepare hdbscan results
-    hdbscan = pd.read_csv('../data/clustering_results/hdbscan_results.csv')
-    hdbscan.drop(hdbscan.columns[0], axis=1, inplace=True)  # drop the first column
-    hdbscan.columns = column_names  # rename columns
-    hdbscan.rename(columns={'WORK/SCHOOL': 'WORK_SCHOOL'}, inplace=True)
+"""
+This function creates and saves locally a boxplot.
+"""
+def boxplot(data, col, where):
+    boxplot_data = [data[data['cluster'] == cluster][col] for cluster in data['cluster'].unique()]
+    plt.figure(figsize=(8, 6))
+    colors = ['#ffcc99', '#ccccff', '#ffffcc', '#ccffe6', '#b3e6ff']
+    boxplot_elements = plt.boxplot(boxplot_data, patch_artist=True, labels=data['cluster'].unique())
+    for patch, color in zip(boxplot_elements['boxes'], colors):
+        patch.set_facecolor(color)
+    plt.xlabel('Cluster')
+    plt.ylabel(col)
+    filename = '../images/' + where + '/hdbscan_' + col + '.png'
+    plt.savefig(filename)
 
-    cols = list(hdbscan.columns[1:37])
-    for col in cols:
-        boxplot_data = [hdbscan[hdbscan['cluster'] == cluster][col] for cluster in hdbscan['cluster'].unique()]
-        plt.figure(figsize=(8, 6))
+
+"""
+This function creates and saves locally a bar plot.
+"""
+def bar_plot(data, col, where):
+    category_counts = data.groupby(['cluster', col]).size().unstack(fill_value=0)
+    plt.figure(figsize=(8, 6))
+    if col == 'mood':
+        colors = ['#ffcc99', '#ccccff', '#ffffcc', '#ccffe6', '#b3e6ff', '#ff704d', '#ffccff', '#80ffaa', '#6666ff', '#c1c1a4', '#ffb3ff', '#99ffeb', '#dfbf9f']
+    else:
         colors = ['#ffcc99', '#ccccff', '#ffffcc', '#ccffe6', '#b3e6ff']
-        boxplot_elements = plt.boxplot(boxplot_data, patch_artist=True, labels=hdbscan['cluster'].unique())
-        for patch, color in zip(boxplot_elements['boxes'], colors):
-            patch.set_facecolor(color)
-        plt.xlabel('Cluster')
-        plt.ylabel(col)
-        filename = '../images/clustering/hdbscan_' + col + '.png'
-        plt.savefig(filename)
+    category_counts.plot(kind='bar', color=colors, figsize=(8, 6))
+    plt.xlabel('Cluster')
+    plt.ylabel(col)
+    filename = '../images/' + where + '/hdbscan_' + col + '.png'
+    plt.savefig(filename)
+
+
+"""
+This function visualizes the training features compared to the assigned cluster from the clustering algorithm.
+"""
+def visualize_training(data):
+    cols = list(data.columns[2:38])
+    for col in cols:
+        boxplot(data, col, 'clustering_training')
+
+
+"""
+This function visualizes the labeling features compared to the assigned cluster from the clustering algorithm.
+"""
+def visualize_labeling(data):
+    cols = list(data.columns[39:])
+    for col in cols:
+        # keep only the data that does not contain NaN values in this specific feature
+        boxplot_data = data.dropna(subset=[col])
+        print("Feature", col, "has", len(boxplot_data), "non-nan values")
+        try:
+            # for the arithmetic features: visualize the clean data with a boxplot
+            boxplot(boxplot_data, col, 'clustering_labeling')
+        except Exception as e1:
+            try:
+                # for the categorical features: visualize the clean data with a bar plot
+                bar_plot(boxplot_data, col, 'clustering_labeling')
+            except Exception as e2:
+                print("Feature", col, "could not be visualized, because", e2)
+
+
+if __name__ == '__main__':
+    # visualize the training features along with the HDBSCAN results
+    data = pd.read_pickle('../data/labeling_visualizations/hdbscan_labeling.pkl')
+    # visualize_training(data)
+
+    # visualize the labeling features along with the HDBSCAN results
+    visualize_labeling(data)
