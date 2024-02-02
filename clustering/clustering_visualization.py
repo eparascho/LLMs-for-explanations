0160f650568a79e34a3999bdfc638dcc6c48df1a
eparascho <eparascho@csd.auth.gr>
2024-02-02 16:28:49 +0200

    statistics on numerical labeling features visualizations

--- clustering/clustering_visualization.py
+++ clustering/clustering_visualization.py
@@ -2,21 +2,24 @@
 This file contains the code for visualizing the clustering results.
 """
 
-
+import warnings
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
-
+from statannotations.Annotator import Annotator
+warnings.filterwarnings('ignore')
 
 """
 This function creates and saves locally a boxplot.
 """
 def boxplot(data, col, where):
-    boxplot_data = [data[data['cluster'] == cluster][col] for cluster in data['cluster'].unique()]
+    data['cluster'] = data['cluster'].astype(str)
     plt.figure(figsize=(8, 6))
-    colors = ['#ffcc99', '#ccccff', '#ffffcc', '#ccffe6', '#b3e6ff']
-    boxplot_elements = plt.boxplot(boxplot_data, patch_artist=True, labels=data['cluster'].unique())
-    for patch, color in zip(boxplot_elements['boxes'], colors):
-        patch.set_facecolor(color)
+    ax = sns.boxplot(x='cluster', y=col, data=data, palette=['#ffcc99', '#ccccff', '#ffffcc', '#ccffe6', '#b3e6ff'])
+    pairs = [(str(i), str(j)) for i in data['cluster'].unique() for j in data['cluster'].unique() if i < j]
+    annotator = Annotator(ax, pairs, data=data, x='cluster', y=col)
+    annotator.configure(test='Mann-Whitney', text_format='star', loc='inside', verbose=2)
+    annotator.apply_and_annotate()
     plt.xlabel('Cluster')
     plt.ylabel(col)
     filename = '../images/' + where + '/kmeans_' + col + '.png'
@@ -27,13 +30,17 @@ def boxplot(data, col, where):
 This function creates and saves locally a bar plot.
 """
 def bar_plot(data, col, where):
-    category_counts = data.groupby(['cluster', col]).size().unstack(fill_value=0)
+    data['cluster'] = data['cluster'].astype(str)
     plt.figure(figsize=(8, 6))
     if col == 'mood':
-        colors = ['#ffcc99', '#ccccff', '#ffffcc', '#ccffe6', '#b3e6ff', '#ff704d', '#ffccff', '#80ffaa', '#6666ff', '#c1c1a4', '#ffb3ff', '#99ffeb', '#dfbf9f']
+        ax = sns.barplot(x='cluster', y=col, data=data, palette=['#ffcc99', '#ccccff', '#ffffcc', '#ccffe6', '#b3e6ff', '#ff704d',
+                                                                 '#ffccff', '#80ffaa', '#6666ff', '#c1c1a4', '#ffb3ff', '#99ffeb', '#dfbf9f'])
     else:
-        colors = ['#ffcc99', '#ccccff', '#ffffcc', '#ccffe6', '#b3e6ff']
-    category_counts.plot(kind='bar', color=colors, figsize=(8, 6))
+        ax = sns.barplot(x='cluster', y=col, data=data, palette=['#ffcc99', '#ccccff', '#ffffcc', '#ccffe6', '#b3e6ff'])
+    pairs = [(str(i), str(j)) for i in data['cluster'].unique() for j in data['cluster'].unique() if i < j]
+    annotator = Annotator(ax, pairs, data=data, x='cluster', y=col)
+    annotator.configure(test='Mann-Whitney', text_format='star', loc='inside', verbose=2)
+    annotator.apply_and_annotate()
     plt.xlabel('Cluster')
     plt.ylabel(col)
     filename = '../images/' + where + '/kmeans_' + col + '.png'
@@ -56,23 +63,23 @@ def visualize_labeling(data):
     cols = list(data.columns[39:])
     for col in cols:
         # keep only the data that does not contain NaN values in this specific feature
-        boxplot_data = data.dropna(subset=[col])
-        print("Feature", col, "has", len(boxplot_data), "non-nan values")
+        visualize_data = data.dropna(subset=[col])
+        print("Feature", col, "has", len(visualize_data), "non-nan values")
         try:
             # for the arithmetic features: visualize the clean data with a boxplot
-            boxplot(boxplot_data, col, 'clustering_labeling')
+            boxplot(visualize_data, col, 'clustering_labeling')
         except Exception:
             try:
                 # for the categorical features: visualize the clean data with a bar plot
-                bar_plot(boxplot_data, col, 'clustering_labeling')
+                bar_plot(visualize_data, col, 'clustering_labeling')
             except Exception as e2:
                 print("Feature", col, "could not be visualized, because", e2)
 
 
 if __name__ == '__main__':
-    # visualize the training features along with the HDBSCAN results
+    # visualize the training features along with the clustering results
     data = pd.read_pickle('../data/labeling_visualizations/kmeans_labeling_processed.pkl')
-    visualize_training(data)
+    # visualize_training(data)
 
-    # visualize the labeling features along with the HDBSCAN results
+    # visualize the labeling features along with the clustering results
     visualize_labeling(data)
