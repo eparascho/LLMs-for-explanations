881f31e846899e6d596f13156100a0adc48f2e01
eparascho <eparascho@csd.auth.gr>
2024-02-06 20:56:22 +0200

    kmeans categories results final

--- clustering/kmeans_local.py
+++ clustering/kmeans_local.py
@@ -11,22 +11,24 @@ from functions.clustering import elbow_method, silhouette_method, perform_cluste
 
 if __name__ == '__main__':
     # read data
-    data = pd.read_pickle('../data/clustering_df.pkl')
+    data = pd.read_pickle('../data/clustering_input/clustering_df_categories.pkl')
 
     # prepare data for clustering (store and then remove id)
     user_id = data['id']
-    data.drop(columns=['id'], inplace=True)
+    dates = data['date']
+    data.drop(columns=['id', 'date'], inplace=True)
 
     # find optimal k for k-means with the elbow method
-    elbow_method('kmeans', data)
+    # elbow_method('kmeans', 'categories', data)
 
     # find optimal k for k-means with the silhouette method
-    silhouette_method('kmeans', data)
+    # silhouette_method('kmeans', 'categories', data)
 
     # perform k-means clustering
     start = time.time()
     print("Clustering with K-means ... ")
     kmeans = KMeans(n_clusters=4, random_state=0, n_init="auto")
-    results = perform_clustering(kmeans, data, user_id)
+    metadata = pd.concat([user_id, dates], axis=1)
+    results = perform_clustering(kmeans, data, metadata)
     print("K-means finished after", time.time() - start)
-    results.to_csv('../data/clustering_results/kmeans_results.csv')
+    results.to_csv('../data/clustering_results/kmeans_results_categories.csv', index=False)
