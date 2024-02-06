9033e3b33a09588f7b22fb2499d7506577b8c91e
eparascho <eparascho@csd.auth.gr>
2024-02-06 20:54:14 +0200

    tsne full & categories final

--- clustering/tsne.py
+++ clustering/tsne.py
@@ -5,11 +5,10 @@ from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     # read data
-    data = pd.read_pickle('../data/clustering_df.pkl')
+    data = pd.read_pickle('../data/clustering_input/clustering_df_categories.pkl')
 
     # prepare data for clustering (store and then remove id)
-    user_id = data['id']
-    data.drop(columns=['id'], inplace=True)
+    data.drop(columns=['id', 'date'], inplace=True)
 
     # visualize data through TSNE
     projection = TSNE().fit_transform(data)
