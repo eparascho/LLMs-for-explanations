3b9b5c0224c7e9e7af099e49ce99d022d4116009
eparascho <eparascho@csd.auth.gr>
2024-02-16 16:23:13 +0200

    histograms for interpretation

--- 3d_visualization/visualization_script.py
+++ 3d_visualization/visualization_script.py
@@ -10,7 +10,7 @@ import plotly.express as px
 
 if __name__ == '__main__':
     # read data
-    df = pd.read_pickle('../data/labeling_visualizations/kmeans_clean_labeling.pkl')
+    df = pd.read_pickle('../data/labeling_visualizations/kmeans_categories_labeling.pkl')
     print(df.columns)
 
     # add jitter factor to the data
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     df['cluster'] = df['cluster'] + np.random.uniform(-jitter_factor, jitter_factor, size=df['cluster'].shape)
 
     # run the 3d plot
-    fig = px.scatter_3d(df, x='steps', y='calories', z='very_active_minutes', color='cluster', color_continuous_scale=px.colors.sequential.Viridis, opacity=0.7)
+    fig = px.scatter_3d(df, x='gender', y='stress_score', z='resting_heart_rate', color='cluster', color_continuous_scale=px.colors.sequential.Viridis, opacity=0.5)
     fig.update_traces(marker=dict(size=3))
-    fig.update_layout(scene=dict(xaxis_title='Steps', yaxis_title='Calories', zaxis_title='Very Active Minutes'))
+    fig.update_layout(scene=dict(xaxis_title='Gender', yaxis_title='Stress score', zaxis_title='Resting heart rate'))
     fig.show()
