bca0ad6cda34479fd95f1161b8cec67b5007f64f
eparascho <eparascho@csd.auth.gr>
2024-06-16 14:52:21 +0300

    clean files & average lime, counterafcatuals and anchor evaluation & lime local explanations for the final instances

--- functions/explainability.py
+++ functions/explainability.py
@@ -10,19 +10,3 @@ def train_test_split_per_user(data, train_size=0.7):
     users_train = users[:slice]
     users_test = users[slice:]
     return data[data.id.isin(users_train)], data[data.id.isin(users_test)]
-
-
-def jaccard_similarity(list1, list2):
-    s1 = set(list1)
-    s2 = set(list2)
-    return len(s1.intersection(s2)) / len(s1.union(s2))
-
-
-def jaccard_distance(usecase):
-    sim = []
-    for l in usecase:
-        i_sim = []
-        for j in usecase:
-            i_sim.append(1-jaccard_similarity(l, j))
-        sim.append(i_sim)
-    return sim
